da9317a596eaedc64e63aac5b943af1d6d6101df
Kousik manna <93304061+kousikmanna24@users.noreply.github.com>
2022-06-28 23:48:47 +0530

    Delete resources directory

--- resources/views/common-component/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-    <div class="row">
-        <div class="col-lg-12" style="text-align:center;">
-            Copyright © GotoAssignmentHelp.com 2022
-        </div>
-    </div>
-</div>
