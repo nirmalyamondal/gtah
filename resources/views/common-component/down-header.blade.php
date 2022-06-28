ad3c9c510c2b4ab54ffa0fb0ad6246a86460c3dd
Kousik manna <93304061+kousikmanna24@users.noreply.github.com>
2022-06-28 22:15:08 +0530

    Add files via upload

---
+++ resources/views/common-component/down-header.blade.php
@@ -0,0 +1,25 @@
+ <!-- Start Page Header -->
+ <div class="page-header">
+         <h1 class="title">Dashboard</h1>
+         <ol class="breadcrumb">
+            <li class="active"></li>
+         </ol>
+         <!-- Start Page Header Right Div -->
+         <div class="right">
+            <div class="btn-group" role="group" aria-label="...">
+               <a href="http://staging.gotoassignmenthelp.com/admin/create-new-page.php" class="btn btn-success">Create New Page</a>
+               <a href="http://staging.gotoassignmenthelp.com/admin/add-testimonial.php" class="btn btn-success">Add Testimonial</a>
+               <!-- <a href="http://staging.gotoassignmenthelp.com/admin/page-add-edit-sidebar.php" class="btn btn-success">Add Side Bar</a> -->
+               <a href="{{ route('category.create') }}" class="btn btn-success">Add Category</a>
+               <a href="http://staging.gotoassignmenthelp.com/admin/page-add-edit-banner.php" class="btn btn-success">Add Banner</a>
+            </div>
+            <div class="btn-group" role="group" aria-label="...">
+               <a href="http://staging.gotoassignmenthelp.com/admin/view-new-page.php" class="btn btn-primary">All Pages</a>
+               <a href="" class="btn btn-light"><i class="fa fa-refresh"></i></a>
+               <!-- <a href="#" class="btn btn-light"><i class="fa fa-search"></i></a>
+                     <a href="#" class="btn btn-light" id="topstats"><i class="fa fa-line-chart"></i></a> -->
+            </div>
+         </div>
+         <!-- End Page Header Right Div -->
+      </div>
+      <!-- End Page Header -->
---
+++ resources/views/common-component/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <div class="row">
+        <div class="col-lg-12" style="text-align:center;">
+            Copyright © GotoAssignmentHelp.com 2022
+        </div>
+    </div>
+</div>
